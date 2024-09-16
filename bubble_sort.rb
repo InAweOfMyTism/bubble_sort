ff35076ffa0f36b8497ef79f21c6b9736f67e1fb
InAweofMyTism <inaweofmytism@gmail.com>
2024-09-16 09:25:23 -0400

    finish

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,7 +1,18 @@
 def bubble_sort(array)
   #do some code
-  
+  for i in 0..array.size do
+    array.each_with_index do |value, index|
+      if index < (array.size - 1)
+        if value > array[index+1]
+          temp = array[index+1]
+          array[index+1] = value
+          array[index] = temp
+        end
+      end
+    end
+  end
+  return array
 end
 
 array = [5,4,3,2,1]
-puts bubble_sort(array)
+p bubble_sort(array)
